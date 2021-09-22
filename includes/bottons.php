d78f7f34d954f7196715942f95ea2685a338d2f4
thomas <46562812+Thomas123456789bobby@users.noreply.github.com>
2021-09-22 12:26:58 +0200

    werk

---
+++ includes/bottons.php
@@ -0,0 +1,11 @@
+<div>
+
+<h1>incude oefeling</h1>
+              <div class="imgs">
+                   <a href="web/camer.php"><img src="../img/cameras_button.png" alt="Cameras" title="Cameras"> </a>
+                   <a href="web/koelkas.php"><img src="../img/koelkasten_button.png" alt="Koankasten" title="Koankasten"> </a>
+                   <a href="web/lamp.php"><img src="../img/lampen_button.png" alt="Lampen" title="Lampen"> </a>
+                   <a href="web/tv.php"><img src="../img/stereo_button.png" alt="Stereo" title="Stereo"> </a>
+                   <a href="web/wasmashine.php"><img src="../img/wasmachines_button.png" alt="Stereo" title="Stereo"> </a>
+                   <a href="index.php"><img src="../img/home.png" alt="Home" title="home"> </a>
+              </div>
