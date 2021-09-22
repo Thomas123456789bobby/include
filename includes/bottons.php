6655a04502278b09b89a95dd581a76ebe3abc194
thomas <46562812+Thomas123456789bobby@users.noreply.github.com>
2021-09-22 15:03:51 +0200

    werk

--- includes/bottons.php
+++ includes/bottons.php
@@ -1,11 +1,19 @@
-<div>
 
+<?php
+
+
+$pagina = $_GET['pagina'];
+
+echo "we zitten op de pagia: $pagina<br>";
+
+?>
+<div>
 <h1>incude oefeling</h1>
               <div class="imgs">
-                   <a href="camer.php"><img src="img/cameras_button.png" alt="Cameras" title="Cameras"> </a>
-                   <a href="koelkas.php"><img src="img/koelkasten_button.png" alt="Koankasten" title="Koankasten"> </a>
-                   <a href="lamp.php"><img src="img/lampen_button.png" alt="Lampen" title="Lampen"> </a>
-                   <a href="tv.php"><img src="img/stereo_button.png" alt="Stereo" title="Stereo"> </a>
-                   <a href="wasmashine.php"><img src="img/wasmachines_button.png" alt="Stereo" title="Stereo"> </a>
-                   <a href="index.php"><img src="img/home.png" alt="Home" title="home"> </a>
+                   <a href="?pagina=camer"><img src="img/cameras_button.png" alt="Cameras" title="Cameras"> </a>
+                   <a href="?pagina=koelkast"><img src="img/koelkasten_button.png" alt="Koankasten" title="Koankasten"> </a>
+                   <a href="?pagina=lamp"><img src="img/lampen_button.png" alt="Lampen" title="Lampen"> </a>
+                   <a href="?pagina=stereo"><img src="img/stereo_button.png" alt="Stereo" title="Stereo"> </a>
+                   <a href="?pagina=wasmachines"><img src="img/wasmachines_button.png" alt="Stereo" title="Stereo"> </a>
+                   <a href="?pagina=home"><img src="img/home.png" alt="Home" title="home"> </a>
               </div>
