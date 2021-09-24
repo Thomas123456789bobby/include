e4c6785c4e428f3c8a4d365aad738fb9d595f2e6
thomas <46562812+Thomas123456789bobby@users.noreply.github.com>
2021-09-24 13:37:59 +0200

    werk

--- includes/bottons.php
+++ includes/bottons.php
@@ -1,4 +1,4 @@
-
+!
 <?php
 
 
@@ -10,10 +10,10 @@ echo "we zitten op de pagia: $pagina<br>";
 <div>
 <h1>incude oefeling</h1>
               <div class="imgs">
-                   <a href="?pagina=camer"><img src="img/cameras_button.png" alt="Cameras" title="Cameras"> </a>
-                   <a href="?pagina=koelkast"><img src="img/koelkasten_button.png" alt="Koankasten" title="Koankasten"> </a>
-                   <a href="?pagina=lamp"><img src="img/lampen_button.png" alt="Lampen" title="Lampen"> </a>
-                   <a href="?pagina=stereo"><img src="img/stereo_button.png" alt="Stereo" title="Stereo"> </a>
-                   <a href="?pagina=wasmachines"><img src="img/wasmachines_button.png" alt="Stereo" title="Stereo"> </a>
-                   <a href="?pagina=home"><img src="img/home.png" alt="Home" title="home"> </a>
+                   <a href="?pagina=camer" style="<?php if($_GET['pagina']=='camer')echo 'border: 5px red solid' ?>"><img src="img/cameras_button.png" alt="Cameras" title="Cameras"> </a>
+                   <a href="?pagina=koelkast" style="<?php if($_GET['pagina']=='koelkast')echo 'border: 5px red solid' ?>"><img src="img/koelkasten_button.png" alt="Koankasten" title="Koankasten"> </a>
+                   <a href="?pagina=lamp" style="<?php if($_GET['pagina']=='lamp')echo 'border: 5px red solid' ?>"><img src="img/lampen_button.png" alt="Lampen" title="Lampen"> </a>
+                   <a href="?pagina=stereo" style="<?php if($_GET['pagina']=='stereo')echo 'border: 5px red solid' ?>"><img src="img/stereo_button.png" alt="Stereo" title="Stereo"> </a>
+                   <a href="?pagina=wasmachines" style="<?php if($_GET['pagina']=='wasmachines')echo 'border: 5px red solid' ?>"><img src="img/wasmachines_button.png" alt="Stereo" title="Stereo"> </a>
+                   <a href="?pagina=home" style="<?php if($_GET['pagina']=='home')echo 'border: 5px red solid' ?>"><img src="img/home.png" alt="Home" title="home"> </a>
               </div>
